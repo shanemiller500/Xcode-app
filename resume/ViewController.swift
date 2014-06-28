3edb6c127fb8db6dd0407fca664c2fc0558f31fc
shane <shanemiller500@gmail.com>
2014-06-28 14:19:08 -0700

    Initial Commit

---
+++ resume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  resume
+//
+//  Created by Shane Miller on 6/28/14.
+//  Copyright (c) 2014 Shane Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
